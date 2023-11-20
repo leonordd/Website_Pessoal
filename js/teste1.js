9e22694dfce9e0a1bdeba19953689b04749ef485
leonordd <leonorddomingues3@gmail.com>
2023-11-20 00:08:56 +0000

    animação hover sobre as imagens da pagina de about e desenho da linha

--- js/teste1.js
+++ js/teste1.js
@@ -56,3 +56,71 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+
+
+
+////teste texto e rato
+let d=10;
+
+let x,y,w,h;
+let c=0;
+
+let textS = 100;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noCursor();
+  background(0);
+  mouseX =-d;
+  mouseY =-d;
+  x=30;
+  y=30;
+  w=100;
+  h=100;
+}
+
+function draw() {
+
+    stroke('#FFFF00');
+    strokeWeight(d);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    if((mouseX>x) && (mouseX<x+w) && (mouseY>y) && (mouseY<=y+h) ){
+        c=255;
+    }
+
+    fill(c);
+    noStroke();
+    rect(x,y,w,h);
+
+    
+    /*stroke(255, 50);
+    fill(255);
+    rect(500,500,300,20, textS);
+    noStroke();
+    fill(0);
+    textSize(textS);
+    //textFont('Avenir');
+    text("jeKnowledge", 500,500,300,textS);*/
+
+}
+
+function mousePressed(){
+  if((mouseX >=0) && (mouseX <window.innerWidth) && (mouseY >0) && (mouseY<window.innerHeight)){
+    background(0);
+  }
+}
+
+/*function textBox(text, posX, posY, wi, he){
+// Drawing the box (not necesary just for visual reference!)
+stroke(255, 50);
+noFill();
+rect(posX,posY, wi,he);
+fill(255);
+textSize(48);
+//textFont("Avenir");
+
+// When you draw text with 4 numbers you create a box that the text fits in
+// x, y position of box followed by width and height of the box
+text(text, posX,posY, wi,he);
+}*/
